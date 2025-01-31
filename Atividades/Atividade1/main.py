0c9be77d695c242106b1672d406f1fbaf9ca465b
Nonolism <ruanvitormoreno@outlook.com>
2025-01-31 17:50:46 -0300

    Aula 2, acessar atributos e atividades

---
+++ Atividades/Atividade1/main.py
@@ -0,0 +1,19 @@
+class Musica:
+    nome: str = ''
+    artista: str = ''
+    duracao: int = 0
+
+musica1 = Musica()
+musica1.nome = 'Ghost'
+musica1.artista = 'Camellia'
+musica1.duracao = 234
+
+musica2 = Musica()
+musica2.nome = 'Padoru Padoru'
+musica2.artista = 'Saber'
+musica2.duracao = 142
+
+musica3 = Musica()
+musica3.nome = 'Caneta Azul'
+musica3.artista = 'manuel Gomes'
+musica3.duracao = 142
--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,11 +1,15 @@
-class Restaurante():
-    nome = ''
-    categoria = ''
-    ativo = False   
-    
+class Restaurante:
+    nome = ""
+    categoria = ""
+    ativo = False
+
+
 restaurante_praca = Restaurante()
+restaurante_praca.nome = "Praça"
+restaurante_praca.categoria = "Gourmet"
+
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+print(restaurante_praca)
