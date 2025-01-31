3bb09f594394c12806f9d106b889673eb071bd72
Nonolism <ruanvitormoreno@outlook.com>
2025-01-31 18:50:04 -0300

    Atividade3, Experimentando com objetos

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -3,13 +3,28 @@ class Restaurante:
     categoria = ""
     ativo = False
 
-
 restaurante_praca = Restaurante()
 restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+restaurante_praca.categoria = "Italiana"
+if restaurante_praca.ativo:
+    print(f"O restauraurante {restaurante_praca.nome} está ativo.")
+else:
+    print(f"O restauraurante {restaurante_praca.nome} está Inativo.")
+print(restaurante_praca)
+print(restaurante_praca.nome)
+restaurante_praca.nome = "Bistro"
 
 restaurante_pizza = Restaurante()
+restaurante_pizza.nome = "Pizza Place"
+restaurante_pizza.categoria = "Fast Food"
+if restaurante_pizza.categoria == "Fast Food":
+    print(True)
+else:
+    print(False)
+restaurante_pizza.ativo = True
+print(f"Nome: {restaurante_praca.nome}\nCategoria: {restaurante_praca.categoria}")
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurante_praca)
+
+categoria = Restaurante.categoria
