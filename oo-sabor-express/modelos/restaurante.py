8a66b02d68d6175404f30944e541c74733cc9c5b
Nonolism <ruanvitormoreno@outlook.com>
2025-02-02 21:00:12 -0300

    Aula 4, Metodo __init__ e metodo __str__

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,30 +1,16 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
+    def __init__(self, nome:str, categoria:str):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Italiana"
-if restaurante_praca.ativo:
-    print(f"O restauraurante {restaurante_praca.nome} está ativo.")
-else:
-    print(f"O restauraurante {restaurante_praca.nome} está Inativo.")
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza Express", "Italiana")
+
+restaurantes = []
+
+print(restaurante_pizza)
 print(restaurante_praca)
-print(restaurante_praca.nome)
-restaurante_praca.nome = "Bistro"
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = "Pizza Place"
-restaurante_pizza.categoria = "Fast Food"
-if restaurante_pizza.categoria == "Fast Food":
-    print(True)
-else:
-    print(False)
-restaurante_pizza.ativo = True
-print(f"Nome: {restaurante_praca.nome}\nCategoria: {restaurante_praca.categoria}")
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-
-categoria = Restaurante.categoria
