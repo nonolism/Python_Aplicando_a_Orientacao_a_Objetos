375f28fa757f1d545987d3f9f76134dbdbafe3e8
Nonolism <ruanvitormoreno@outlook.com>
2025-02-14 19:05:56 -0300

    Aula8 Mais sobre Property

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -2,24 +2,25 @@ class Restaurante:
     restaurantes = []
     
     def __init__(self, nome:str, categoria:str):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-    @property #transforma o método em uma propriedade
-    #necessário transformar o self.ativo em privado (colocando um _ no começo) 
-    #senão o método não funciona por causa do decorator @property 
-    # que não aceita atributos públicos
+    @property 
     def ativo(self):
+        '''transforma o método em uma propriedade
+        necessário transformar o self.ativo em privado (colocando um _ no começo) 
+        senão o método não funciona por causa do decorator @property 
+        que não aceita atributos públicos'''
         return '⌧' if self._ativo else '☐'
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
